f2b484e1234b1a2a3f62d25b5a182891c0653e6c
stigmate <arthurhemono@yahoo.fr>
2020-03-18 15:34:59 +0100

    requeste_table_name

--- merge_bdd.c
+++ merge_bdd.c
@@ -1,87 +1,133 @@
 #include <string.h>
 #include <mysql/mysql.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DB_HOST "localhost"
 #define DB_USER "root"
 #define DB_PASS "mysql"
 #define DB_NAME "testdb"
+#define STRING_SIZE 50
 
 int main(int argc, char **argv)
 { 
-  MYSQL_STMT *stmt;  
-  MYSQL *con = mysql_init(NULL);
+	MYSQL_STMT *stmt;  
+	MYSQL *con = mysql_init(NULL);
+	MYSQL_BIND bind[1];
+	my_ulonglong  affected_rows;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+	
    
-  int param_count;
-  const char* query;
+	char str_data[STRING_SIZE];
+	unsigned long str_length;
+	int param_count;
+	const char* query;
+	unsigned int num_fields;
+	unsigned int i;
 
-  if (con == NULL) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      exit(1);
-  }
+	if (mysql_library_init(0, NULL, NULL)) {
+    		fprintf(stderr, "could not initialize MySQL client library\n");
+    		exit(1);
+	}
 
-  if (mysql_real_connect(con, DB_HOST, DB_USER, DB_PASS, 
+	if (con == NULL) 
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		exit(1);
+	}
+
+	if (mysql_real_connect(con, DB_HOST, DB_USER, DB_PASS, 
           DB_NAME, 0, NULL, 0) == NULL) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }  
-/*
-  if (mysql_query(con, "USE testdb")) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }*/
-  // char* today = DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s');
-  // char* folder = '/var/lib/mysql-files/';
-  // char* file = 'test';
-  // char* ext = '.csv';
-  query = "SELECT * FROM testtable INTO OUTFILE '/var/lib/mysql-files/DATE_FORMAT(NOW()\'_\%Y_\%m\').csv' FIELDS ENCLOSED BY '\"' TERMINATED BY ';' ESCAPED BY '\"' LINES TERMINATED BY '\r\n';";   
-  stmt = mysql_stmt_init(con);
-  if(!stmt)
-  {
-      fprintf(stderr, " mysql_stmt_init(), out of memory\n");
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}  
+
+
+	query = "SELECT * FROM testdb";   
+	stmt = mysql_stmt_init(con);
+	
+	if(!stmt)
+	{
+		fprintf(stderr, " mysql_stmt_init(), out of memory\n");
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}
   
-  if(mysql_stmt_prepare(stmt, query, strlen(query)))
-  {
-  	fprintf(stderr, " mysql_stmt_prepare(), INSERT failed\n");
-  	fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
-  	exit(0);
-  }
+	if(mysql_stmt_prepare(stmt, query, strlen(query)))
+	{
+		fprintf(stderr, " mysql_stmt_prepare(), INSERT failed\n");
+		fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
+		exit(0);
+	}
 
 
-  fprintf(stdout, " prepare, SELECT successful\n");
+	fprintf(stdout, " prepare, SELECT successful\n");
 
-  param_count= mysql_stmt_param_count(stmt);
-  fprintf(stdout, " total parameters in SELECT: %d\n", param_count);
+	param_count = mysql_stmt_param_count(stmt);
+	fprintf(stdout, " total parameters in SELECT: %d\n", param_count);
 
-  if (param_count != 0) /* validate parameter count */
-  {
-    fprintf(stderr, " invalid parameter count returned by MySQL\n");
-    exit(0);
-  }
+	if (param_count != 1) 
+	{
+		fprintf(stderr, " invalid parameter count returned by MySQL\n");
+		exit(0);
+	}
+/*
+	memset(bind, 0, sizeof(bind));
 
-  if (mysql_stmt_execute(stmt))
-  {
-  	fprintf(stderr, " mysql_stmt_execute(), 1 failed\n");
-  	fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
-  	exit(0);
-  }
-	printf("la requete a été executé");
+	bind[0].buffer_type = MYSQL_TYPE_STRING;
+	bind[0].buffer = (char*)str_data;
+	bind[0].buffer.length = STRING_SIZE;
+	bind[0].is_null = 0;
+	bind[0].length = &str_length; 
 
+	if(mysql_stmt_bind_param(stmt, bind))
+	{
+		fprintf(stderr, " mysql_stmt_bind_param() failed\n");
+		fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
+		exit(0);
+	}
+	
+	strncpy(str_data, DB_NAME, STRING_SIZE);
+	str_length = strlen(str_data);
 
+	if (mysql_stmt_execute(stmt))
+	{
+  		fprintf(stderr, " mysql_stmt_execute(), 1 failed\n");
+  		fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
+  		exit(0);
+	}*/
 
+	result = mysql_list_tables(con, NULL);
+	
+	if(result)
+	{
+		num_fields = mysql_num_fields(result);
+		printf("result is empty\n");
+	}else{
+		fprintf(stderr,"Error: %s\n", mysql_error(&stmt));
+	}
 
+	while ((row = mysql_fetch_row(result)))
+	{
+   		unsigned long *lengths;
+   		lengths = mysql_fetch_lengths(result);
+   		for(i = 0; i < num_fields; i++)
+   		{
+       			printf("%.*s ", (int) lengths[i],
+              		row[i] ? row[i] : "NULL");
+   		}
+   		printf("\n");
+	}
+	
+	printf("la requete a été executé\n");
 
-  mysql_stmt_close(stmt);
-  mysql_close(con);
-  exit(0);
+  	mysql_stmt_close(stmt);
+  	mysql_close(con);
+	mysql_library_end();
+  	return EXIT_SUCCESS;
 }
 
